d841bf2ce2527e3d9422dffb79da19d38ad4f525
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2020-11-21 12:46:07 -0500

    ex4

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@
 // import the needed node_modules.
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const { top50 } = require('./data/top50');
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
@@ -18,6 +20,37 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+  
+  //exercise 2
+  .get("/top50/", (req, res)=>{
+      res.status(200).json({status:200, data: top50});
+  })
+  //exercise 3
+  .get("/top50/songs/:number", (req, res)=>{
+    if(req.params.number<=50 && req.params.number>=0){
+      res.status(200).json({status:200, data: top50[req.params.number-1]});
+    }
+    else{
+      res.status(404).json({status:404, message:"Song not found"});
+    }
+    
+  })
+
+  //exercise 4
+  .get("/top50/artist/:artistName", (req, res)=>{
+    const artistObject=top50.filter(element=>{
+      if(element.artist.toLowerCase()===req.params.artistName.toLowerCase()){
+        return element;
+      }
+    })
+    console.log(artistObject);
+    if(artistObject.length===0){
+      res.status(404).json({status:404, message:"Song not found"})
+    }
+    else{
+      res.status(200).json({status:200, data:artistObject})
+    }
+  })
 
   // add new endpoints here ☝️
   // ---------------------------------
