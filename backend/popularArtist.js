add43a35cd95cd30b14b8472b2d4f9a54b41909f
ferielnabet <fnabet@ipconnex.com>
2020-11-21 16:45:11 -0500

    update

--- backend/popularArtist.js
+++ backend/popularArtist.js
@@ -4,18 +4,19 @@ const data = require("./data/top50");
 
 const popularArtist =  (req, res) =>{
 
-
-    const countOccurrences = arr => arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
-    const artistList = data.top50.map(elm => elm.artist);
-
-    let max = Math.max(...Object.values(countOccurrences(artistList)));
     
-    function getKeyByValue(object, value) { 
+    const countOccurrences = arr => arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {});
+
+    const getKeyByValue = (object, value) =>{ 
         return Object.keys(object).find(key =>  
                 object[key] === value); 
     } 
 
-    popular = getKeyByValue(countOccurrences(artistList) , max)
+
+    const artistList = data.top50.map(elm => elm.artist);
+    let max = Math.max(...Object.values(countOccurrences(artistList)));
+    
+    popular  = getKeyByValue(countOccurrences(artistList) , max)
 
     const songsList = data.top50.filter(song => song.artist == popular);
 
