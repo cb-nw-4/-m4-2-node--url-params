5b3ee1bbc88a69e1e0bd4b9b495b450780bacfd6
ferielnabet <fnabet@ipconnex.com>
2020-11-21 12:17:55 -0500

    endpoint /top50 Doe

--- backend/server.js
+++ backend/server.js
@@ -2,35 +2,47 @@
 
 // import the needed node_modules.
 const express = require("express");
+const app = express();
 const morgan = require("morgan");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const data = require('./data/top50');
 
-express()
-  // Below are methods that are included in express(). We chain them for convenience.
-  // --------------------------------------------------------------------------------
 
-  // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
-  .use(morgan("tiny"))
-  .use(bodyParser.json())
+// Below are methods that are included in express(). We chain them for convenience.
+// --------------------------------------------------------------------------------
 
-  // Any requests for static files will go into the public folder
-  .use(express.static("public"))
+// This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
+app.use(morgan("tiny"))
+app.use(bodyParser.json())
 
-  // Nothing to modify above this line
-  // ---------------------------------
-  // add new endpoints here 👇
+// Any requests for static files will go into the public folder
+app.use(express.static("public"))
 
-  // add new endpoints here ☝️
-  // ---------------------------------
-  // Nothing to modify below this line
+// Nothing to modify above this line
+// ---------------------------------
+// add new endpoints here 👇
 
-  // this is our catch all endpoint.
-  .get("*", (req, res) => {
-    res.status(404).json({
-      status: 404,
-      message: "This is obviously not what you are looking for.",
-    });
+app.get('/top50', (req, res) =>{
+
+  res.status(200).json({
+    status: 200,
+    data: data.top50
   })
 
-  // Node spins up our server and sets it to listen on port 8000.
-  .listen(8000, () => console.log(`Listening on port 8000`));
+})
+
+
+// add new endpoints here ☝️
+// ---------------------------------
+// Nothing to modify below this line
+
+// this is our catch all endpoint.
+app.get("*", (req, res) => {
+  res.status(404).json({
+    status: 404,
+    message: "This is obviously not what you are looking for.",
+  });
+})
+
+// Node spins up our server and sets it to listen on port 8000.
+app.listen(8000, () => console.log(`Listening on port 8000`));
